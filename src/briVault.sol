bf46d44537c644eaf01515eba2340dd0c5714d2f
Bryanmankind <shegeemankind@gmail.com>
2025-08-08 03:28:53 +0100

    update

--- src/briVault.sol
+++ src/briVault.sol
@@ -24,10 +24,12 @@ contract BriVault is ERC4626, Ownable {
     uint256 public eventEndDate;
     uint256 public  stakedAmount;
     uint256 public totalAssetsShares;
-    uint256 public winner;
+    string public winner;
     uint256 public finalizedVaultAsset;
     uint256 public totalWinnerShares;
     bool public _setWinner;
+    uint256 public winnerCountryId;
+
 
     // minimum amount to join in.
     uint256 public  minimumAmount; 
@@ -51,14 +53,14 @@ contract BriVault is ERC4626, Ownable {
     error eventNotStarted();
 
     event deposited (address indexed _depositor, uint256 _value);
-    event CountriesSet(uint256[48] country);
-    event joinedEvent(address user, uint256 _countryId);
+    event CountriesSet(string[48] country);
+    event WinnerSet (string winnerSet);
+    event joinedEventb(address user, uint256 _countryId);
     event Withdraw (address user, uint256 _amount);  
 
     mapping (address => uint256) public depositAsset;
-    mapping(uint256 => string) public teamIndexToCountry;
-    mapping (address => address) public userToCountry;
-    mapping (address => mapping (uint256 => string)) public userSharesToCountry;
+    mapping (address => string) public userToCountry;
+    mapping(address => mapping(uint256 => uint256)) public userSharesToCountry;
     
 
     constructor (IERC20 _asset, uint256 _participationFeeBsp, uint256 _eventStartDate, address _participationFeeAddress, uint256 _minimumAmount, uint256 _eventEndDate) ERC4626 (_asset) ERC20("BriTechLabs", "BTT") Ownable(msg.sender) {
@@ -78,30 +80,33 @@ contract BriVault is ERC4626, Ownable {
         _;
     }
 
-    function setCountry(string[48] memory countries) public onlyOwner {
-        for (uint256 i = 0; i < countries.length; ++i) {
-            countryToTeamIndex[i + 1] = countries[i];
-            teams[i] = countryToTeamIndex[i + 1];
-        }
-        emit CountriesSet(countries);
+ function setCountry(string[48] memory countries) public onlyOwner {
+    for (uint256 i = 0; i < countries.length; ++i) {
+        teams[i] = countries[i];
     }
+    emit CountriesSet(countries);
+}
+
 
     function setWinner(uint256 countryId) public onlyOwner returns (bool) {
         if (block.timestamp <= eventEndDate) {
             revert eventNotEnded();
         }
 
-        if (countryToTeamIndex[countryId] != 0) {
+        if (bytes(teams[countryId]).length == 0) {
             revert invalidCountry();
         }
 
-        winner = countryToTeamIndex[countryId];
-
-        _getWinnerShares();
+        winnerCountryId = countryId;
+        winner = teams[countryId];
 
         _setWinner = true;
 
+        _getWinnerShares();
+
         return _setWinner;
+
+        emit WinnerSet (winner);
     }
 
     function setFinallizedVaultBalance () public onlyOwner returns (uint256) {
@@ -112,19 +117,23 @@ contract BriVault is ERC4626, Ownable {
         return finalizedVaultAsset = address(this).balance;
     }
 
-    function getWinner () public view returns (uint256) {
+    function getWinner () public view returns (string memory) {
         return winner;
     }
 
-    function getTeamIndexForCountry(uint256 countryId) external view returns (uint256) {
-        return countryToTeamIndex[countryId];
+    function getCountry(uint256 countryId) external view returns (string memory) {
+         if (bytes(teams[countryId]).length == 0) {
+            revert invalidCountry();
+        }
+
+        return teams[countryId];
     }
 
     function _getWinnerShares () internal returns (uint256) {
 
         for (uint256 i = 0; i < usersAddress.length; ++i){
-            address user = usersAddress[i];
-           totalWinnerShares += userSharesToCountry[user][winner];
+            address user = usersAddress[i]; 
+           totalWinnerShares += userSharesToCountry[user][winnerCountryId];
         }
         return totalWinnerShares;
     }
@@ -165,44 +174,38 @@ contract BriVault is ERC4626, Ownable {
     /**
     @dev allows users to join the event 
     */
-    function joinEvent (uint256 countryId) public returns (uint256 participantShares) {
-        if (depositAsset[msg.sender] == 0) {
-            revert noDeposit();
-        }
-
-        bool valid = false;
-
-        for (uint256 i = 0; i <= 48; ++i) {
-            if (countryToTeamIndex[i] == countryId) {
-                valid = true;
-                break;
-            }
-            if (!valid){
-                revert invalidCountry(); 
-            }
-        }
-        require(block.timestamp <= eventStartDate, eventStarted());
-
-        uint256 stakeAsset = depositAsset[msg.sender];
-
-        participantShares = _convertToShares(stakeAsset);
-
-        stakedAmount += depositAsset[msg.sender];
-
-        userToCountry[msg.sender] = countryId;
-
-        userSharesToCountry[msg.sender][countryId] = participantShares;
-
-        depositAsset[msg.sender] = 0;
-
-        usersAddress.push(msg.sender);
-
-        numberOfParticipants++;
-
-        _mint(msg.sender, participantShares);
-
-        emit joinedEvent (msg.sender, countryId);
+function joinEvent(uint256 countryId) public returns (uint256 participantShares) {
+    if (depositAsset[msg.sender] == 0) {
+        revert noDeposit();
     }
+
+    // Ensure countryId is a valid index in the `teams` array
+    if (countryId >= teams.length) {
+        revert invalidCountry();
+    }
+
+    require(block.timestamp <= eventStartDate, "eventStarted");
+
+    uint256 stakeAsset = depositAsset[msg.sender];
+
+    participantShares = _convertToShares(stakeAsset);
+
+    stakedAmount += stakeAsset;
+
+    // Assign user to country
+    userToCountry[msg.sender] = teams[countryId];
+    userSharesToCountry[msg.sender][countryId] = participantShares;
+
+    depositAsset[msg.sender] = 0;
+
+    usersAddress.push(msg.sender);
+    numberOfParticipants++;
+
+    _mint(msg.sender, participantShares);
+
+    emit joinedEvent(msg.sender, countryId);
+}
+
 
     /**
     @dev cancell participation 
@@ -227,8 +230,8 @@ contract BriVault is ERC4626, Ownable {
             revert eventNotEnded();
         }
 
-        if (userToCountry[msg.sender] != winner) {
-            revert didNotWin();
+         if (keccak256(abi.encodePacked(userToCountry[msg.sender])) != keccak256(abi.encodePacked(winner))) {
+             revert didNotWin();
         }
 
         uint256 assetToWithdraw = (shares * finalizedVaultAsset * 1e18) / totalWinnerShares;
